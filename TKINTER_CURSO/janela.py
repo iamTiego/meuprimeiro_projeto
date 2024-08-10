77cc6f5826f62fd2a8aad12f4238795c8e5ac30f
Diego Assis <144186367+iamTiego@users.noreply.github.com>
2024-08-09 22:18:44 -0300

    Minha primeira Janela

---
+++ TKINTER_CURSO/janela.py
@@ -0,0 +1,14 @@
+from tkinter import *
+
+#cores
+cor1='#140c33' #azul
+
+janela = Tk()
+janela.title('Olá Mundo')
+janela.geometry('600x250')
+janela.config(bg=cor1)
+
+janela.iconphoto((False, PhotoImage file='programador.png'))
+
+
+janela.mainloop()
